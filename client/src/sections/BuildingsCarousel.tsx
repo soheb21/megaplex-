6185b5ef8ab8bd6353949cd1a979567e8a9bc8a6
unknown <ansarishoeb53@gmail.com>
2026-02-14 23:16:13 +0530

    fixed errors

--- client/src/sections/BuildingsCarousel.tsx
+++ client/src/sections/BuildingsCarousel.tsx
@@ -1,8 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
 
 type Building = {
   title: string;
